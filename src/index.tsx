d2628843a8d29e7ae3cf2eaac013342d8021b2ff
Hyeonjong <nianelo4@gmail.com>
2025-08-08 21:26:43 +0900

    feat: add box component

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,17 @@
+import { Text, View, type TextProps, type ViewProps } from 'react-native';
+
 export function multiply(a: number, b: number): number {
   return a * b;
+}
+
+type BoxProps = ViewProps & {
+  textProps?: Omit<TextProps, 'children'>;
+};
+
+export function Box({ children, textProps, ...otherProps }: BoxProps) {
+  return (
+    <View {...otherProps}>
+      <Text {...textProps}>{children}</Text>
+    </View>
+  );
 }
