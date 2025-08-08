c7ece0d6ecec43c596d2276aeb60617e553649fb
Hyeonjong <nianelo4@gmail.com>
2025-08-08 21:52:28 +0900

    test: write box tests

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,1 +1,18 @@
-it.todo('write a test');
+import { render } from '@testing-library/react-native';
+import { Box } from '../index';
+
+function App() {
+  return <Box>Lorem ipsum dolor sit amet</Box>;
+}
+
+test('basic test 1', () => {
+  const { getByText } = render(<App />);
+
+  expect(getByText('Lorem ipsum dolor sit amet')).toBeOnTheScreen();
+});
+
+test('basic test 2', () => {
+  const { getAllByText } = render(<App />);
+
+  expect(getAllByText('Lorem ipsum dolor sit amet')).toHaveLength(1);
+});
